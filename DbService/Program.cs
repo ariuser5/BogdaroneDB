bf8d0b82bfa34e42c849899aa5f82754ecbfc3d4
Darius <darius-emanuel.ciuverca@guehring.de>
2022-03-24 23:02:35 +0200

    Added service project and service host project.

---
+++ DbService/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace DbService
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			using(var host = new ServiceHost(typeof(DbAccess.UsersService)))
+			{
+				host.Open();
+				Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+				Console.ReadLine();
+			}
+
+		}
+
+
+	}
+}
